3032b32bad5aa50b0aa9ace74f1168846691d510
shirarei24 <periophthalmus24@gmail.com>
2024-04-21 19:02:30 +0900

    feat: add simple todo list feat

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,60 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Form,
+    response::{Html, IntoResponse},
+    routing::{get, post},
+    Router,
+};
+
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+use askama::Template;
+#[derive(Template)]
+#[template(path = "tasks.html")]
+struct TasksTemplate<'a> {
+    tasks: &'a Vec<String>,
+}
+
+#[derive(Template)]
+#[template(path = "todo_list.html")]
+#[allow(dead_code)]
+struct TodoListTemplate<'a> {
+    tasks: &'a Vec<String>,
+}
+
+lazy_static! {
+    static ref TASKS: Mutex<Vec<String>> = Mutex::new(vec![]);
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .nest_service("/assets", tower_http::services::ServeDir::new("assets"))
+        .route("/", get(show_tasks))
+        .route("/add", post(add_task));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, world!</h1>")
+async fn show_tasks() -> Html<String> {
+    let tasks = TASKS.lock().unwrap();
+    let template = TodoListTemplate { tasks: &tasks };
+    Html(template.render().unwrap())
+}
+
+async fn add_task(Form(input): Form<AddTask>) -> impl IntoResponse {
+    let mut tasks = TASKS.lock().unwrap();
+    tasks.push(input.task);
+    let template = TasksTemplate { tasks: &tasks };
+    Html(template.render().unwrap())
+}
+
+#[derive(serde_derive::Deserialize)]
+struct AddTask {
+    task: String,
 }
